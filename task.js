585dd7355e003d7a078478d0b415f23cd0e7a500
KevinHu101 <89288267+KevinHu101@users.noreply.github.com>
2021-12-06 12:18:43 -0800

    Update task.js

--- task.js
+++ task.js
@@ -1,1 +1,12 @@
-const inputbox = document.querySelector("");
+//getting all required elements
+const inputBox = document.querySelector(".inputfield input");
+const addBtn = document.querySelector(".inputfield button");
+
+inputBox.onkeyup = ()=>{
+    let userData = inputBox.value; //getting user entered value
+    if(userData.trim() != 0){ //if user values aren't only space
+        addBtn.classList.add("active"); //active the add button
+    }else{
+        addBtn.classList.remove("active");
+    }
+}
