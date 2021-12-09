4eca1fdf809df93dcdcdf2ba7a2d1a51b8549b45
Kevin <kevin2500365109@gmail.com>
2021-12-08 23:21:56 -0800

    task

--- task.js
+++ task.js
@@ -1,6 +1,7 @@
 //getting all required elements
 const inputBox = document.querySelector(".inputfield input");
 const addBtn = document.querySelector(".inputfield button");
+const todoList = document.querySelector(".tasks");
 
 inputBox.onkeyup = ()=>{
     let userData = inputBox.value; //getting user entered value
@@ -9,4 +10,32 @@ inputBox.onkeyup = ()=>{
     }else{
         addBtn.classList.remove("active");
     }
+
+    //if user click on the add button
+    addBtn.onclick = ()=>{
+        let userData = inputBox.value; //getting user entered value
+        let getLocalStorage = localStorage.getItem("New Todo"); //getting localstorage
+        if(getLocalStorage == null){ //if localStorage is null
+            listArr = []; //creating blank array
+        }else{
+            listArr = JSON.parse(getLocalStorage);
+        }
+        listArr.push(userData);
+        localStorage.setItem("New Todo", JSON.stringify(listArr)); //transforming js object into a json string
+        showTasks(); //calling showTasks function
+    }
+}
+
+function showTasks(){
+    let getLocalStorage = localStorage.getItem("New Todo"); //getting localstorage
+    if(getLocalStorage == null){ //if localStorage is null
+        listArr = []; //creating blank array
+    }else{
+        listArr = JSON.parse(getLocalStorage);
+    }
+    let newLiTag = '';
+    listArr.array.forEach(element => {
+        newLiTag = `<li> ${element} <span id=level>emergcey level</span><ion-icon name="trash-outline" id="delete"></ion-icon></li>`;
+    });
+    todoList.innerHTML = newLiTag; //adding new li tag inside ul tag
 }
