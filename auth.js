eda0d5f7f370310f9968e3fa61df7ae4f1cd9df3
Kevin <kevin2500365109@gmail.com>
2022-04-07 00:22:15 -0700

    Improved authentication. Login in improved. Initialized emulators. Next work on sign up.

--- auth.js
+++ auth.js
@@ -1,3 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { 
+    getAuth, 
+    signInWithEmailAndPassword, 
+    connectAuthEmulator
+} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+import { getFirestore} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyAYbHvFRpO1vwjo8zTeGlQOmgTETfO_5u4",
+authDomain: "cvtechteam-c44a2.firebaseapp.com",
+projectId: "cvtechteam-c44a2",
+storageBucket: "cvtechteam-c44a2.appspot.com",
+messagingSenderId: "724830760584",
+appId: "1:724830760584:web:f103595593f11349ed793d",
+measurementId: "G-WF0BXB7HLT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+connectAuthEmulator(auth, "http://localhost:9099");
+
 //sign up
 const signupForm = document.querySelector('#register');
 signupForm.addEventListener('submit', (e) =>{
@@ -7,7 +39,23 @@ signupForm.addEventListener('submit', (e) =>{
     const email = signupForm['signupEmail'].value;
     const password = signupForm['signupPassword'].value;
 
-    //sign up the user
-    auth.createUserWithEmailAndPassword(email, password).then(cred =>
-        console.log(cred));
+    try {
+//sign up the user
+const userCredential = signInWithEmailAndPassword(auth, email, password);
+console.log(userCredential.user);
+    }
+    catch(error) {
+    console.log(error);
+    }
 })
+
+
+export const showLoginError = (error) => {
+    divLoginError.style.display = "block";
+    if (error.code == AuthErrorCodes.INVALID_PASSWORD) {
+        lblLoginErrorMessage.innerHTML = 'Wrong Password. Try again.';
+    }
+    else{
+        lblLoginErrorMessage.innerHTML = 'Error: ${error.message}';
+    }
+}
