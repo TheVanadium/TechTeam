94507141c62e753859564a55b76deedb29e136b1
Kevin <kevin2500365109@gmail.com>
2022-08-06 14:16:12 -0700

    Improved stl page. Improved user interface. Going to work on adding, deleting, and interacting with task next. Plan to improve user register soon which will include adding profile.

--- public/task.js
+++ public/task.js
@@ -43,7 +43,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const user = auth.currentUser;
-const colRef = collection(db, 'request');
+const allRequests = collection(db, 'request');
 
 
 
@@ -78,15 +78,14 @@ setDoc(doc(citiesRef, "SF"), {
 
 
         // const q = query(collection(db, "cities"), where("capital", "==", true));
-        const q = query(collection(db, "cities"));
+//         const q = query(collection(db, "cities"));
 
-const querySnapshot = await getDocs(q);
-querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-    console.log(doc.id, " => ", doc.data());
-    console.log(doc.data().name);
-});
-
+// const querySnapshot = await getDocs(q);
+// querySnapshot.forEach((doc) => {
+//   // doc.data() is never undefined for query doc snapshots
+//     console.log(doc.id, " => ", doc.data());
+//     console.log(doc.data().name);
+// });
 
 
 
@@ -96,10 +95,59 @@ querySnapshot.forEach((doc) => {
 const inputBox = document.querySelector('.inputfield input');
 const addBtn = document.querySelector('.inputfield button');
 const todoList = document.querySelector('.tasks');
+const list = document.getElementsByClassName('tags');
+const networkRequest = query(allRequests, where('option', '==', 'Network'));
+const printerRequest = query(allRequests, where('option', '==', 'Printer'));
+const otherRequest = query(allRequests, where('option', '==', 'Other'));
+const chromeRequest = query(allRequests, where('option', '==', 'Chrome'));
+const printerCats = document.getElementById('c1Tasks');
 
+
+
+
+const querySnapshot = await getDocs(printerRequest);
+querySnapshot.forEach((doc) => {
+
+    var id = doc.data().id;
+    var fullName = doc.data().fullName;
+    var studentName;
+    if(doc.data().studentName!="") {studentName=`<br/><span>Student name: </span>${doc.data().studentName},`;} else {studentName="";}
+    var email = `<br/><span>Email: </span>${doc.data().email},`;
+    var phoneNumber;
+    if(doc.data().phoneNumber!="") {phoneNumber=`<br/><span>Phone number: </span>${doc.data().phoneNumber},`;} else {phoneNumber="";}
+    var description = `<br/><span>Description: </span>${doc.data().description}`;
 
 
     
+    let newListItem = document.createElement('li');
+    newListItem.innerHTML=`<li class="tags">${id}, ${fullName} 
+    <span id=level>emergency</span><ion-icon name="trash-outline" id="delete"></ion-icon>
+    ${studentName}
+    ${email}
+    ${phoneNumber}
+    ${description}
+    </li>`;
+
+        newListItem.addEventListener('click', function() 
+        {
+        this.classList.toggle('tags-read-more');
+        }) 
+        
+    printerCats.appendChild(newListItem);
+})
+
+
+
+for(var i=0; i<list.length; i++)
+{
+    list[i].addEventListener('click', function() {
+        this.classList.toggle('tags-read-more');
+}) 
+}
+
+    
+
+//detects input
 inputBox.onkeyup = () =>{
     let userData = inputBox.value; //getting user entered value
     if(userData.trim() != 0){ //if user values aren't only space
@@ -107,6 +155,7 @@ inputBox.onkeyup = () =>{
     }else{
         addBtn.classList.remove("active");
     }
+}
 
     //if user click on the add button
     addBtn.onclick = () =>{
@@ -121,7 +170,6 @@ inputBox.onkeyup = () =>{
         localStorage.setItem("New Todo", JSON.stringify(listArr)); //transforming js object into a json string
         showTasks(); //calling showTasks function
     }
-}
 
 function showTasks(){
     let getLocalStorage = localStorage.getItem("New Todo"); //getting localstorage
