b96cd36a2c0eab1b26ba25ef4388cab959878253
Kevin <kevin2500365109@gmail.com>
2022-12-02 23:31:31 -0800

    Fixed bugs. Added thumb up animation.

--- public/js/learn.js
+++ public/js/learn.js
@@ -1,3 +1,11 @@
-function thumb(){
-    document.querySelector('.card__info').style.add
+thumbClicked();
+
+//This function assign a click event, an animation, to all of the thumb icon
+function thumbClicked() {
+    let thumb = document.querySelectorAll(".thumbUp");
+    for (let i = 0; i < thumb.length; i++) {
+        thumb[i].addEventListener("click", function () {
+            thumb[i].classList.toggle("thumbclicked");
+        });
+    }
 }
